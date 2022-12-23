f8e4499245c5f652dc8d3e6abe1d221aaa86e2d9
anderson-nunes <andersonribeironunes@hotmail.com>
2022-12-23 14:09:46 -0300

    usando Context no projeto e organizando pastas

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,6 @@
 import { differenceInSeconds } from "date-fns";
-import { useContext, useEffect, useState } from "react";
-import { CyclesContext } from "../..";
+import { useContext, useEffect } from "react";
+import { CyclesContext } from "../../../../contexts/CyclesContext";
 import { CountContainer, Separator } from "./styles";
 
 export function Countdown() {
